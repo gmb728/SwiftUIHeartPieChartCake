142065516aedb04cfa921b078b0ec5c97d81e00f
Sophia <gmb728@gmail.com>
2020-03-20 16:23:11 +0800

    Initial Commit

---
+++ SwiftUIHeartPieChartCake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIHeartPieChartCake
+//
+//  Created by Chang Sophia on 3/20/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
